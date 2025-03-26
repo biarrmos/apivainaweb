de10ce9e28f968f31a45ffd87649a338ecb13af4
Bia Ramos <beatriz.ramosdeluca@gmail.com>
2025-03-26 15:13:50 -0300

    adicionando requirements.txt e CORS

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import sqlite3
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 def init_db():
     with sqlite3.connect("database.db") as conn:
